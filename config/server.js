5d18ee7b0ace06ca3031c66795bc9df01460e0af
kywingky <agismuhammadabduh296@gmail.com>
2025-05-31 22:37:45 +0700

    fix: parse PUBLIC_URL  manually

--- config/server.js
+++ config/server.js
@@ -3,3 +3,11 @@ module.exports = ({ env }) => ({
     keys: env("APP_KEYS").split(","),
   },
 });
+module.exports = ({ env }) => ({
+  url: env("PUBLIC_URL", "http://localhost:1337"),
+  host: "0.0.0.0",
+  port: env.int("PORT", 1337),
+  app: {
+    keys: env("APP_KEYS").split(","),
+  },
+});
