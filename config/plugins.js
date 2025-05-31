a49a9a9131bb0b1d9dfdbda3e060a70df5cd0b34
kywingky <agismuhammadabduh296@gmail.com>
2025-05-31 22:08:22 +0700

    fix: add JWT secret for users-permissions

--- config/plugins.js
+++ config/plugins.js
@@ -10,3 +10,10 @@ module.exports = ({ env }) => ({
     },
   },
 });
+module.exports = ({ env }) => ({
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET"),
+    },
+  },
+});
